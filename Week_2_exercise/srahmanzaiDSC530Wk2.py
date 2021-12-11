a54743314cc6c2171431aa85973d3307f3b75402
srahmanzai <saimarahmanzai@msn.com>
2021-12-10 19:11:32 -0600

    Srahamanzai Week2 Assignment

---
+++ Week_2_exercise/srahmanzaiDSC530Wk2.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("Hello World! I wonder why that is always the default coding text to start with")
+
+
+# In[5]:
+
+
+5 + 10
+
+
+# In[6]:
+
+
+20 - 5
+
+
+# In[7]:
+
+
+3 * 5
+
+
+# In[8]:
+
+
+15 / 3
+
+
+# In[9]:
+
+
+"Hello!" + " " + "Looking forward to learn Python!"
+
+
+# In[10]:
+
+
+Golf_Forecast = ["Tiger Woods", [65,66,70,65], "Master Championship 2022", "Winner!"]
+Golf_Forecast
+
+
+# In[11]:
+
+
+Golf_Forecast.append("Augusta GA")
+
+
+# In[12]:
+
+
+Golf_Forecast
+
+
+# In[13]:
+
+
+Golf_USA_Olympics = ("Nelli Korda", "Gold Medalist", 2020, "Tokyo")
+Golf_USA_Olympics
+
+
+# In[ ]:
+
+
+
+
